3b0420a2c03f229acdec758b4ddab8bdffaea31b
Chad Fennell <fenne035@umn.edu>
2013-11-10 21:34:03 -0600

    hmm, looks like heroku is not seeing these directories

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ DplaEtl::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.autoload_paths += %W(#{config.root}/lib)
 end
