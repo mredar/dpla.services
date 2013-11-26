cc4d2fb96440dc50c544ae4d8ea1b035358e0639
Chad Fennell <fenne035@umn.edu>
2013-11-26 12:26:06 -0600

    accidental regeneration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107155159) do
+ActiveRecord::Schema.define(version: 20131114183745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,5 @@ ActiveRecord::Schema.define(version: 20131107155159) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
+  
 end
