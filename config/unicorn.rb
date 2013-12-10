862fa0f686f37494960a8ca114fe9f7932d4655e
Chad Fennell <fenne035@umn.edu>
2013-12-10 16:56:23 -0600

    we need multiple workers to handle tests that call open-uri of services that call open-uri

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+root = '/home/fenne035/dev/dpla/dpla.etl'
+
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/unicorn/unicorn.log"
+stdout_path "#{root}/unicorn/unicorn.log"
+
+# listen "/tmp/unicorn.dplahub.sock"
+listen 8083, :tcp_nopush => true
+worker_processes 2
+timeout 30
