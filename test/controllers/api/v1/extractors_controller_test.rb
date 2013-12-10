be4e4be676179b2b92d6a253af3afea01863f7d4
Chad Fennell <fenne035@umn.edu>
2013-12-10 17:09:58 -0600

    for now, I just want to make sure my mock extraction point and mock service are up

--- test/controllers/api/v1/extractors_controller_test.rb
+++ test/controllers/api/v1/extractors_controller_test.rb
@@ -2,12 +2,11 @@ require "minitest_helper"
 require 'json'
 require 'open-uri'
 
-describe "Extractors Controler" do 
-  url = 'http://localhost:8083/api/v1/extract?endpoint=http://reflections.mndigital.org/oai/oai2.php&endpoint_type=oai_dc&query_params=%26verb%3DListRecords&access_token=fefa907fca5c9e5fb2c7e01d437138b0&cache_response=true'
+describe "Extractors Controller" do 
   it 'loads a basic extraction' do
+    url = 'http://localhost:8083/api/v1/extract?endpoint=http://reflections.mndigital.org/oai/oai2.php&endpoint_type=oai_dc&query_params=%26verb%3DListRecords&access_token=fefa907fca5c9e5fb2c7e01d437138b0&cache_response=true'    
     open(url) do |e|
-      raw_response = e.read
       e.status[0].must_equal "200"
     end
-  end   
+  end
 end
