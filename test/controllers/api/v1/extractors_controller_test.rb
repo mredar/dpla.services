8862b92fcbaeec3c0917b16969a75414c683c13d
Chad Fennell <fenne035@umn.edu>
2013-12-11 15:54:27 -0600

    extractors controller test checks our mock endpoint

--- test/controllers/api/v1/extractors_controller_test.rb
+++ test/controllers/api/v1/extractors_controller_test.rb
@@ -1,10 +1,10 @@
-require "minitest_helper"
+require "test_helper"
 require 'json'
 require 'open-uri'
 
 describe "Extractors Controller" do 
-  it 'loads a basic extraction' do
-    url = 'http://localhost:8083/api/v1/extract?endpoint=http://reflections.mndigital.org/oai/oai2.php&endpoint_type=oai_dc&query_params=%26verb%3DListRecords&access_token=fefa907fca5c9e5fb2c7e01d437138b0&cache_response=true'    
+  it 'ensures the mock OAI service is running' do
+    url = 'http://localhost:8083/api/v1/extract?endpoint=http://localhost:3000/oai/show&endpoint_type=oai_dc&query_params=%26verb%3DListRecords&access_token=fefa907fca5c9e5fb2c7e01d437138b0&cache_response=true'    
     open(url) do |e|
       e.status[0].must_equal "200"
     end
