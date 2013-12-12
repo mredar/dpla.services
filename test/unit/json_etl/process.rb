e9d7a3140c9b10747e49ba5ad249f0aca43d8c6e
Chad Fennell <fenne035@umn.edu>
2013-12-12 15:47:33 -0600

    adding a general purpose config file for dev, stag and prod

--- test/unit/json_etl/process.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TestJsonEtlProcess < ActiveSupport::TestCase
-  def test_geonames_postal
-    locations = ['United States', 'Minnesota', 'Minneapolis', 'Hennepin County']
-    json = JsonEtl::Transform::Processors.geonames_postal(locations, {}, 'libsys')
-    assert_equal({}, json)    
-  end
-end
