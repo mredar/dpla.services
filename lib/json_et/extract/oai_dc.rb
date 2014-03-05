7e14bc1fd0058acdea6e952a70f03f54db6bb005
Chad Fennell <fenne035@umn.edu>
2014-03-05 13:54:50 -0600

    refactoring params a bit, remove the retry method - the client handles retries (via sidekiq)

--- lib/json_et/extract/oai_dc.rb
+++ lib/json_et/extract/oai_dc.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'open-uri'
 require 'service_log'
+require 'cgi'
+require 'uri'
 
 module JsonEt
   module Extract
@@ -17,46 +19,56 @@ module JsonEt
       end
 
       def build_url(params)
-        prefix = (defined?(params['batch_param']) && !params['batch_param'].nil?) ? "?#{params['batch_param']}" : '?metadataPrefix=oai_dc'
-        query = params[:query_params] ? CGI::unescape(params[:query_params]) : nil
-        "#{params['endpoint']}#{prefix}#{query}"
+
+        query_params = CGI::unescape(params[:query_params])
+        parsed_query = CGI.parse(query_params)
+
+        params[:batch_param] ||= nil
+
+        if (params[:batch_param] && params[:batch_param] != '')
+          query_params = URI.encode_www_form(parsed_query.except('metadataPrefix'))
+          batch_param = "&#{CGI::unescape(params[:batch_param])}"
+        else
+          batch_param = nil
+        end
+
+        "#{params['endpoint']}?#{query_params}#{batch_param}"
       end
 
       def get_remote_data(url, params)
-        attempt(5, 5) {
-         open(url) {|e|
-            raw_response = e.read
-            results = Hash.from_xml(raw_response)
-            code = e.status[0]
-            next_batch_params = nil
+        open(url, :read_timeout => 600 ) {|e|
+          raw_response = e.read
+          results = Hash.from_xml(raw_response)
+          code = e.status[0]
+          next_batch_params = nil
 
-            # Pull out the resumption token so that the pipeline does not need to do so
-            if defined?(results["OAI_PMH"]["ListRecords"]["resumptionToken"])
-              if (results["OAI_PMH"]["ListRecords"]["resumptionToken"])
-                next_batch_params = CGI::escape("resumptionToken=#{results["OAI_PMH"]["ListRecords"]["resumptionToken"]}")
-              end
+          # Pull out the resumption token so that the pipeline does not need to do so
+          if defined?(results["OAI_PMH"]["ListRecords"]["resumptionToken"])
+            if (results["OAI_PMH"]["ListRecords"]["resumptionToken"])
+              next_batch_params = CGI::escape("resumptionToken=#{results["OAI_PMH"]["ListRecords"]["resumptionToken"]}")
             end
+          end
 
-            # Extractor should always return an array of items
-            if results["OAI_PMH"]["ListRecords"]
-              service_log.info("Processing a Record Set")
-              results = results["OAI_PMH"]["ListRecords"]['record']
-            else
-              service_log.info("Processing an Extraction Set")
-              results = results["OAI_PMH"]["ListSets"]['set']
-            end
+          # Extractor should always return an array of items
+          if results["OAI_PMH"]["ListRecords"]
+            service_log.info("Processing a Record Set")
+            results = results["OAI_PMH"]["ListRecords"]['record']
+          elsif results["OAI_PMH"]["ListSets"]
+            service_log.info("Processing an Extraction Set")
+            results = results["OAI_PMH"]["ListSets"]['set']
+          end
 
-            service_log.info("Fetching fresh extraction from params '#{next_batch_params}'")
-            errors = (defined?(results['OAI_PMH']['error'])) ? results['OAI_PMH']['error'] : nil
-            response = {
-              'next_batch_params' => [next_batch_params],
-              'original_params' => params,
-              'records' => results,
-              'http_status_code' => code,
-              'errors' => errors
-              }
-            return response
-          }
+          service_log.info("Next Batch: '#{next_batch_params}'")
+          errors = (defined?(results['OAI_PMH']['error'])) ? results['OAI_PMH']['error'] : nil
+
+          response = {
+            'next_batch_params' => [next_batch_params],
+            'original_params' => params,
+            'records' => results,
+            'http_status_code' => code,
+            'errors' => errors
+            }
+          return response
         }
       end
     end
