7b5b764a0836ca88fa94bcdb942f8efcb1ff451f
Chad Fennell <fenne035@umn.edu>
2014-03-13 11:11:24 -0500

    add service log to productoin

--- app/models/concerns/service_log.rb
+++ app/models/concerns/service_log.rb
@@ -2,7 +2,7 @@ module ServiceLog
   def service_log
     if Rails.env.test?
       @@service_log ||= Logger.new("#{Rails.root}/log/service-test.log")
-    elsif Rails.env.development?
+    elsif Rails.env.development? || Rails.env.production?
       @@service_log ||= Logger.new("#{Rails.root}/log/service.log")
     end
   end
