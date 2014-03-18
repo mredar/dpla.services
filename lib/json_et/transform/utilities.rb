3eae9dd23e744cca1701520fa57d8f9828018b26
Chad Fennell <fenne035@umn.edu>
2014-03-18 16:35:48 -0500

    reverting. multiple selectors already gives you this: 'try this, and this and this', which works better for cases where you want a selector to simple fail if the predicate is not present

--- lib/json_et/transform/utilities.rb
+++ lib/json_et/transform/utilities.rb
@@ -169,8 +169,7 @@ class Hash
     end
   end
 
-  # Start by supporting a few XPath predicates. If the item is not an array,
-  # just return it. This helps when an field can be either an array or a string
+  # Start by supporting a few XPath predicates.
   def fetch_predicate(item, matches)
     pred = matches[2]
     data = item[matches[1]]
@@ -182,8 +181,6 @@ class Hash
       else
         data[pred.to_i]
       end
-    else
-      return data
     end
   end
 end
